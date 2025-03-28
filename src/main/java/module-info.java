1c5d22d8c93d509ff392f26a061bf334e1a5b9b6
Gabriel Vicent <rodriguezisaac876@gmail.com>
2025-03-28 17:05:55 -0400

    Primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module cat.soft.com.catasoft {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens cat.soft.com.catasoft to javafx.fxml;
+    exports cat.soft.com.catasoft;
+}
